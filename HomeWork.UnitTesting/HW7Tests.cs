0f0c3457b9749310a7d5cc58820c81f8213a0668
HasanOmar1 <r3domar1@gmail.com>
2025-04-24 20:39:34 +0300

    added HW7

--- HomeWork.UnitTesting/HW7Tests.cs
+++ HomeWork.UnitTesting/HW7Tests.cs
@@ -7,8 +7,74 @@ namespace HomeWork.UnitTesting
     public class HW7Tests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Brothers_Num1Is13Num2Is123_ReturnsTrue()
         {
+            // Arrange
+            bool actual;
+            int num1 = 13, num2 = 123;
+
+            // Act
+            actual = HW7.Brothers(num1, num2);
+
+            // Assert
+            Assert.IsTrue(actual , "Should return true but got " + actual);
+        }
+
+        [TestMethod]
+        public void Brothers_Num1Is7Num2Is71237_ReturnsTrue()
+        {
+            // Arrange
+            bool actual;
+            int num1 = 7, num2 = 71237;
+
+            // Act
+            actual = HW7.Brothers(num1, num2);
+
+            // Assert
+            Assert.IsTrue(actual, "Should return true but got " + actual);
+        }
+
+        [TestMethod]
+        public void Brothers_Num1Is14Num2Is15_ReturnsFalse()
+        {
+            // Arrange
+            bool actual;
+            int num1 = 14, num2 = 15;
+
+            // Act
+            actual = HW7.Brothers(num1, num2);
+
+            // Assert
+            Assert.IsFalse(actual, "Should return false but got " + actual);
+        }
+
+        [TestMethod]
+        public void Brothers_Num1IsMinus13Num2Is10_ReturnsFalse()
+        {
+            // Arrange
+            bool actual;
+            int num1 = -13, num2 = 10;
+
+            // Act
+            actual = HW7.Brothers(num1, num2);
+
+            // Assert
+            Assert.IsFalse(actual, "Should return false but got " + actual);
+        }
+
+
+        [TestMethod]
+        public void Brothers_Num1Is13Num2IsMinus10_ReturnsFalse()
+        {
+            // Arrange
+            bool actual;
+            int num1 = 13, num2 = -10;
+
+            // Act
+            actual = HW7.Brothers(num1, num2);
+
+            // Assert
+            Assert.IsFalse(actual, "Should return false but got " + actual);
         }
     }
 }
--- HomeWork/HW7.cs
+++ HomeWork/HW7.cs
@@ -8,7 +8,7 @@ namespace HomeWork
 {
     public class HW7
     {
-        public static bool brothers(int num1 , int num2)
+        public static bool Brothers(int num1 , int num2)
         {
             if(num1 <= 0 || num2 <= 0) return false;
 
