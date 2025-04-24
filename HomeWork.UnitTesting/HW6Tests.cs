4e2f9b346451bfe39339f4e31e8c5a555a3cae5e
HasanOmar1 <r3domar1@gmail.com>
2025-04-24 21:20:55 +0300

    added HW6

--- HomeWork.UnitTesting/HW6Tests.cs
+++ HomeWork.UnitTesting/HW6Tests.cs
@@ -7,8 +7,102 @@ namespace HomeWork.UnitTesting
     public class HW6Tests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CalcAverageOfIntArr_Null_ReturnsDoubleMinValue()
         {
+
+            // Arrange
+            double actual;
+            double expected = double.MinValue;
+
+            // Act
+            actual = HW6.CalcAverageOfIntArr(null);
+
+            // Assert
+
+            Assert.AreEqual(expected, actual , "Expected double.MinValue but got " + actual);
+
+
+
+        }
+
+        [TestMethod]
+        public void CalcAverageOfIntArr_EmptyArr_ReturnsDoubleMinValue()
+        {
+
+            // Arrange
+            double actual;
+            double expected = double.MinValue;
+            int[] arr = { };
+
+            // Act
+            actual = HW6.CalcAverageOfIntArr(arr);
+
+            // Assert
+
+            Assert.AreEqual(expected, actual, "Expected double.MinValue but got " + actual);
+
+
+
+        }
+
+        [TestMethod]
+        public void CalcAverageOfIntArr_ArrWithDifferentValues_Returns25DotZero()
+        {
+
+            // Arrange
+            double actual;
+            double expected = 25.0;
+            int[] arr = {10,20,30,40};
+
+            // Act
+            actual = HW6.CalcAverageOfIntArr(arr);
+
+            // Assert
+
+            Assert.AreEqual(expected, actual, "Expected " + expected + " but got " + actual);
+
+
+
+        }
+
+        [TestMethod]
+        public void CalcAverageOfIntArr_ArrWithSameValuesOf9_Returns9DotZero()
+        {
+
+            // Arrange
+            double actual;
+            double expected = 9.0;
+            int[] arr = { 9,9,9,9,9,9,9,9};
+
+            // Act
+            actual = HW6.CalcAverageOfIntArr(arr);
+
+            // Assert
+
+            Assert.AreEqual(expected, actual, "Expected " + expected + " but got " + actual);
+
+
+
+        }
+
+        [TestMethod]
+        public void CalcAverageOfIntArr_ArrWithOneMember6_Returns6DotZero()
+        {
+
+            // Arrange
+            double actual;
+            double expected = 6.0;
+            int[] arr = { 6 };
+
+            // Act
+            actual = HW6.CalcAverageOfIntArr(arr);
+
+            // Assert
+
+            Assert.AreEqual(expected, actual, "Expected " + expected + " but got " + actual);
+
+
+
         }
     }
 }
--- HomeWork/HW6.cs
+++ HomeWork/HW6.cs
@@ -8,7 +8,17 @@ namespace HomeWork
 {
     public class HW6
     {
-        // if the arr is empty or the argument is null - return double.MinValue
+        public static double CalcAverageOfIntArr(int[] arr)
+        {
+            if (arr == null || arr.Length == 0) return double.MinValue;
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+                sum += arr[i];
+
+            return (double) sum / arr.Length;
+
+        }
 
     }
 }
