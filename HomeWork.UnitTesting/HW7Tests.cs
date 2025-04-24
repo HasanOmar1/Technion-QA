2f6e9ccc24d530cbdcb8cf3b5aefc665a06443f8
HasanOmar1 <r3domar1@gmail.com>
2025-04-24 21:51:27 +0300

    added DataRow to HW7

--- HomeWork.UnitTesting/HW7Tests.cs
+++ HomeWork.UnitTesting/HW7Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace HomeWork.UnitTesting
@@ -6,75 +7,96 @@ namespace HomeWork.UnitTesting
     [TestClass]
     public class HW7Tests
     {
+        //[TestMethod]
+        //public void Brothers_Num1Is13Num2Is123_ReturnsTrue()
+        //{
+        //    // Arrange
+        //    bool actual;
+        //    int num1 = 13, num2 = 123;
+
+        //    // Act
+        //    actual = HW7.Brothers(num1, num2);
+
+        //    // Assert
+        //    Assert.IsTrue(actual, "Should return true but got " + actual);
+        //}
+
+        //[TestMethod]
+        //public void Brothers_Num1Is7Num2Is71237_ReturnsTrue()
+        //{
+        //    // Arrange
+        //    bool actual;
+        //    int num1 = 7, num2 = 71237;
+
+        //    // Act
+        //    actual = HW7.Brothers(num1, num2);
+
+        //    // Assert
+        //    Assert.IsTrue(actual, "Should return true but got " + actual);
+        //}
+
+        //[TestMethod]
+        //public void Brothers_Num1Is14Num2Is15_ReturnsFalse()
+        //{
+        //    // Arrange
+        //    bool actual;
+        //    int num1 = 14, num2 = 15;
+
+        //    // Act
+        //    actual = HW7.Brothers(num1, num2);
+
+        //    // Assert
+        //    Assert.IsFalse(actual, "Should return false but got " + actual);
+        //}
+
+        //[TestMethod]
+        //public void Brothers_Num1IsMinus13Num2Is10_ReturnsFalse()
+        //{
+        //    // Arrange
+        //    bool actual;
+        //    int num1 = -13, num2 = 10;
+
+        //    // Act
+        //    actual = HW7.Brothers(num1, num2);
+
+        //    // Assert
+        //    Assert.IsFalse(actual, "Should return false but got " + actual);
+        //}
+
+
+        //[TestMethod]
+        //public void Brothers_Num1Is13Num2IsMinus10_ReturnsFalse()
+        //{
+        //    // Arrange
+        //    bool actual;
+        //    int num1 = 13, num2 = -10;
+
+        //    // Act
+        //    actual = HW7.Brothers(num1, num2);
+
+        //    // Assert
+        //    Assert.IsFalse(actual, "Should return false but got " + actual);
+        //}
+
         [TestMethod]
-        public void Brothers_Num1Is13Num2Is123_ReturnsTrue()
+        [DataRow(13,123,true,DisplayName = "Brothers_Num1Is13Num2Is123_ReturnsTrue")]
+        [DataRow(7,71237,true, DisplayName = "Brothers_Num1Is7Num2Is71237_ReturnsTrue")]
+        [DataRow(14, 15, false, DisplayName = "Brothers_Num1Is14Num2Is15_ReturnsFalse")]
+        [DataRow(-13, 10, false, DisplayName = "Brothers_Num1IsMinus13Num2Is10_ReturnsFalse")]
+        [DataRow(13, -10, false, DisplayName = "Brothers_Num1Is13Num2IsMinus10_ReturnsFalse")]
+
+
+        public void Brothers(int num1,int num2,bool expected )
         {
             // Arrange
             bool actual;
-            int num1 = 13, num2 = 123;
 
             // Act
             actual = HW7.Brothers(num1, num2);
 
             // Assert
-            Assert.IsTrue(actual , "Should return true but got " + actual);
-        }
+            Assert.AreEqual(expected,actual, "Should return " + expected + " but got " + actual);
 
-        [TestMethod]
-        public void Brothers_Num1Is7Num2Is71237_ReturnsTrue()
-        {
-            // Arrange
-            bool actual;
-            int num1 = 7, num2 = 71237;
-
-            // Act
-            actual = HW7.Brothers(num1, num2);
-
-            // Assert
-            Assert.IsTrue(actual, "Should return true but got " + actual);
-        }
-
-        [TestMethod]
-        public void Brothers_Num1Is14Num2Is15_ReturnsFalse()
-        {
-            // Arrange
-            bool actual;
-            int num1 = 14, num2 = 15;
-
-            // Act
-            actual = HW7.Brothers(num1, num2);
-
-            // Assert
-            Assert.IsFalse(actual, "Should return false but got " + actual);
-        }
-
-        [TestMethod]
-        public void Brothers_Num1IsMinus13Num2Is10_ReturnsFalse()
-        {
-            // Arrange
-            bool actual;
-            int num1 = -13, num2 = 10;
-
-            // Act
-            actual = HW7.Brothers(num1, num2);
-
-            // Assert
-            Assert.IsFalse(actual, "Should return false but got " + actual);
-        }
-
-
-        [TestMethod]
-        public void Brothers_Num1Is13Num2IsMinus10_ReturnsFalse()
-        {
-            // Arrange
-            bool actual;
-            int num1 = 13, num2 = -10;
-
-            // Act
-            actual = HW7.Brothers(num1, num2);
-
-            // Assert
-            Assert.IsFalse(actual, "Should return false but got " + actual);
         }
     }
 }
