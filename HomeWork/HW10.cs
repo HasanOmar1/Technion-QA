2476db54753196e8cb2a581afe00d12b0c999596
HasanOmar1 <r3domar1@gmail.com>
2025-05-07 18:32:17 +0300

    removed extra

--- HomeWork/HW10.cs
+++ HomeWork/HW10.cs
@@ -12,7 +12,6 @@ namespace HomeWork
         public static bool IsPrime(int num)
         {
             if (num < 2) return false;
-            if (num == 2) return true;
             for (int i = 2; i < num; i++)
                 if (num % i == 0) return false;
 
