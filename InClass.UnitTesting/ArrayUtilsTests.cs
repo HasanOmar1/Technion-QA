f960455e46986863dafdc3567949add4796f350b
Hasan Omar <r3domar1@gmail.com>
2025-05-08 09:26:04 +0300

    added ArrayUtils and tests

--- InClass.UnitTesting/ArrayUtilsTests.cs
+++ InClass.UnitTesting/ArrayUtilsTests.cs
@@ -7,8 +7,108 @@ namespace InClass.UnitTesting
     public class ArrayUtilsTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetEvenItemsInIntArray_ArrayWithEvenNums_ReturnsEvenArr()
         {
+
+            // Arrange
+            int[] arrToTest = { -3, 1, 10, 3, 8, 20 };
+            int[] expected = { 10,8,20 };
+            int[] actual;
+
+            ArrayUtils myCode = new ArrayUtils();
+
+            // Act
+            actual = myCode.GetEvenItemsInIntArray(arrToTest);
+
+            // Assert
+
+            Assert.AreEqual(expected.Length, actual.Length, "Bug in array length");
+
+            for (int i = 0; i < expected.Length; i++)
+                Assert.AreEqual(expected[i], actual[i], "Problem in item #" + i);
+        }
+
+        [TestMethod]
+        public void GetEvenItemsInIntArray_ArrayWithOutEvenNums_ReturnsEmptyArr()
+        {
+
+            // Arrange
+            int[] arrToTest = { -3, 1, 101, 3, 81, 201 };
+            int[] expected = { };
+            int[] actual;
+
+            ArrayUtils myCode = new ArrayUtils();
+
+            // Act
+            actual = myCode.GetEvenItemsInIntArray(arrToTest);
+
+            // Assert
+
+            Assert.AreEqual(expected.Length, actual.Length, "Bug in array length");
+
+            for (int i = 0; i < expected.Length; i++)
+                Assert.AreEqual(expected[i], actual[i], "Problem in item #" + i);
+        }
+
+        [TestMethod]
+        public void GetEvenItemsInIntArray_Null_ReturnsNull()
+        {
+
+            // Arrange
+            int[] arrToTest = null;
+            int[] expected = null;
+            int[] actual;
+
+            ArrayUtils myCode = new ArrayUtils();
+
+            // Act
+            actual = myCode.GetEvenItemsInIntArray(arrToTest);
+
+            // Assert
+
+            if(expected == null && actual == null)
+            {
+                Assert.IsTrue(true);
+                return;
+            }
+
+
+            if (expected == null && actual != null)
+                Assert.Fail("I was expecting null but didnt get null");
+
+            if (expected != null && actual == null)
+                Assert.Fail("I was NOT expecting null but i got null");
+
+
+            // if the code comes here then we know that expected and actual are not null
+
+            Assert.AreEqual(expected.Length, actual.Length, "Bug in array length");
+
+            for (int i = 0; i < expected.Length; i++)
+                Assert.AreEqual(expected[i], actual[i], "Problem in item #" + i);
+        }
+
+
+        [TestMethod]
+        public void GetEvenItemsInIntArray_EmptyArray_ReturnsEmptyArr()
+        {
+
+            // Arrange
+            int[] arrToTest = { };
+            int[] expected = { };
+            int[] actual;
+
+            ArrayUtils myCode = new ArrayUtils();
+
+            // Act
+            actual = myCode.GetEvenItemsInIntArray(arrToTest);
+
+            // Assert
+
+            Assert.AreEqual(expected.Length, actual.Length, "Bug in array length");
+
+            for (int i = 0; i < expected.Length; i++)
+                Assert.AreEqual(expected[i], actual[i], "Problem in item #" + i);
         }
     }
 }
--- InClass/ArrayUtils.cs
+++ InClass/ArrayUtils.cs
@@ -2,11 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace InClass
 {
     public class ArrayUtils
     {
+        
+        // if array is empty -  return empty array
+        // if array is null  - return null
+        public int[] GetEvenItemsInIntArray(int[] arr)
+        {
+
+
+            if (arr == null)
+                return null;
+
+
+            if ( arr.Length == 0)
+                return new int[] { };
+
+            int countEvens = 0;
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] % 2 == 0)
+                    countEvens++;
+
+            int[] evenArr = new int[countEvens];
+
+            if (evenArr.Length == 0)
+                return new int[] {} ;
+
+            int index = 0;
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] % 2 == 0)
+                    evenArr[index++] = arr[i];
+
+            return evenArr;
+
+
+        }
     }
 }
