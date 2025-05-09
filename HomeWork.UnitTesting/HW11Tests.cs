7c7bede94714346ecbce5ae64f9d4bcaf123153b
HasanOmar1 <r3domar1@gmail.com>
2025-05-09 22:08:03 +0300

    added HW11

---
+++ HomeWork.UnitTesting/HW11Tests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace HomeWork.UnitTesting
+{
+    [TestClass]
+    public class HW11Tests
+    {
+        [TestMethod]
+        [DataRow("c" , true , DisplayName = "IsSpecial_SingleCharacter_ReturnsTrue")]
+        [DataRow("abc", true, DisplayName = "IsSpecial_StringContainsOnlySmallLettersOnce_ReturnsTrue")]
+        [DataRow("abca", false, DisplayName = "IsSpecial_StringContainsOnlySmallLettersMoreThanOnce_ReturnsFalse")]
+
+        [DataRow("aB", false, DisplayName = "IsSpecial_StringContainsCapitalLetters_ReturnsFalse")]
+        [DataRow("abcde&a", false, DisplayName = "IsSpecial_StringWithNotChar_ReturnsFalse")]
+        [DataRow("", false, DisplayName = "IsSpecial_EmptyString_ReturnsFalse")]
+        [DataRow(null, false, DisplayName = "IsSpecial_Null_ReturnsFalse")]
+
+
+
+
+        public void IsSpecial(string str , bool expected)
+        {
+
+            // Arrange
+            bool actual;
+
+            // Act
+            actual = HW11.IsSpecial(str);
+
+            // Assert
+            Assert.AreEqual(expected, actual , "Bug: Expected " + expected + " but got " + actual);
+        }
+    }
+}
---
+++ HomeWork/HW11.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    public class HW11
+    {
+
+        public static bool IsSpecial(string str)
+        {
+            if(str == null || str.Length == 0) return false;
+
+            for(int i = 0; i < str.Length; i++)
+            {
+                if (str[i] < 'a' || str[i] > 'z') return false;
+
+                bool isThere = false;
+
+                for(int j = 0; j < str.Length; j++)
+                {
+                    if (str[i] == str[j] && i != j)
+                        isThere = true ;
+                }
+                if (isThere) return false;
+            }
+
+
+            return true;
+        }
+    }
+}
