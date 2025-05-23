1e496aa8e66a49beee0c868db46c7d28b36fbd95
HasanOmar1 <r3domar1@gmail.com>
2025-05-23 20:46:09 +0300

    added HW15Box (not finished)

---
+++ HomeWork/HW15Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    public class HW15Box
+    {
+        private string color;
+        private int len;
+        private int width;
+        private int height;
+        private double weight;
+
+        public HW15Box(string color,int len ,int width, int height , double weight)
+        {
+            this.color = color;
+            this.len = len;
+            this.width = width;
+            this.height = height;
+            this.weight = weight;
+        }
+
+        public HW15Box(string color , double weight)
+        { 
+            this.color = color;
+            this.weight = weight;
+
+        }
+
+
+    }
+}
