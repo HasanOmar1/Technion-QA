834220451d5ea7ad1673ea1b35a65d0636af695c
Hasan Omar <r3domar1@gmail.com>
2025-05-28 10:27:05 +0300

    added HW16

---
+++ HomeWork.UnitTesting/HW16Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HomeWork.UnitTesting
+{
+    [TestClass]
+    public class HW16Tests
+    {
+        [TestMethod]
+        public void buildK_validArr_ReturnsValidData(int[] a , int k , int[] expected)
+        {
+            // Arrange
+            int[] actual;
+
+            // Act
+            actual = HW16.buildK(a,k);
+
+            // Assert
+            Assert.AreEqual(expected.Length / k, actual.Length);
+           
+        }
+    }
+}
---
+++ HomeWork/HW16.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    public class HW16
+    {
+
+        public static int[] buildK(int[] a , int k)
+        {
+            int[] arr = new int[a.Length * k];
+            int count = 0;
+
+            for (int i = 0;  i < a.Length;  i++)
+            {
+                for (int j = 0; j < k; j++)
+                    arr[count++] = a[i];
+            }
+
+            return arr;
+        }
+    }
+}
