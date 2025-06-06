18a4f76ef18504fdbe9f304a6909c9ea2a9b0aff
HasanOmar1 <r3domar1@gmail.com>
2025-06-06 12:06:27 +0300

    Added HW 18 (not finished)

---
+++ HomeWork.UnitTesting/HW18ClockTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HomeWork.UnitTesting
+{
+    [TestClass]
+    public class HW18ClockTests
+    {
+        [DataTestMethod]
+        [DynamicData("ClockTestGenerator", DynamicDataSourceType.Method)]
+        public void Clock_DynamicData(int hour, int min, int expected)
+        {
+            // Arrange
+            HW18Clock myCode = new HW18Clock(hour, min);
+            int actual;
+
+
+            // Act
+            actual = myCode.GetInterval();
+
+            // Assert
+            Assert.AreEqual(expected, actual, "Should have gotten " + expected + " but got " + actual);
+        }
+
+        public static object[][] ClockTestGenerator()
+        {
+            return new[]
+            {
+                new object[] {20 , 45 , 195},
+                new object[] {23 , 59 , 1},
+                new object[] {24 , 0 , 0},
+                new object[] {31 , 5 , 0 },
+                new object[] {0 , 0 , 1440}
+            };
+        }
+    }
+}
---
+++ HomeWork/HW18Clock.cs
@@ -0,0 +1,49 @@
+﻿namespace HomeWork
+{
+    public class HW18Clock
+    {
+        private int hour; // hours 0-23
+        private int min; // minutes 0-59
+
+
+        public HW18Clock(int hour, int min)
+        {
+            this.hour = hour;
+            this.min = min;
+
+        }
+
+        public int getHour()
+        {
+            return this.hour;
+        }
+
+        public int getMin()
+        {
+            return this.min;
+        }
+
+
+
+
+        public int GetInterval()
+        {
+            int TwentyFourInMinutes = 1440; // 24*60
+            int currentHourInMinutes = this.hour * 60;
+            int currentTimeInMinutes = currentHourInMinutes + this.min;
+
+            if (currentTimeInMinutes > TwentyFourInMinutes)
+                return 0;
+
+            return TwentyFourInMinutes - currentTimeInMinutes;
+
+
+        }
+
+
+    }
+
+
+
+
+}
