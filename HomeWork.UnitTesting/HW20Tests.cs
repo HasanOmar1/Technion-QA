4f66b238f67fca38ed59b5469183d96d1f9995ec
HasanOmar1 <r3domar1@gmail.com>
2025-06-08 21:54:52 +0300

    Added HW20 without xml

---
+++ HomeWork.UnitTesting/HW20Tests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HomeWork.UnitTesting
+{
+    [TestClass]
+    public class HW20Tests
+    {
+        [TestMethod]
+        public void IsSpecial_StringIsAAAaa_ReturnsFalse()
+        {
+            // Arrange
+            bool actual, expected;
+            string str = "AAAaa";
+
+            // Act
+            expected = false;
+            actual = HW20.IsSpecial(str);
+
+
+            // Assert
+            Assert.AreEqual(expected, actual, "Expected " + expected + " but got " + actual);
+        }
+
+
+        [DataTestMethod]
+        [DataRow(null, false, DisplayName = "IsSpecial_StrIsNull_ReturnsFalse")]
+        [DataRow("", false, DisplayName = "IsSpecial_StrIsEmpty_ReturnsFalse")]
+        [DataRow("12@34%!56", false, DisplayName = "IsSpecial_StrIsNotLetters_ReturnsFalse")]
+        [DataRow("AbC23cBa1@", true, DisplayName = "IsSpecial_StrIsValid_ReturnsTrue")]
+        [DataRow("AbC23CBa1@", false, DisplayName = "IsSpecial_StrIsInValid_ReturnsFalse")]
+        public void IsSpecial_DataRow(string str, bool expected)
+        {
+            // Arrange
+            bool actual;
+
+            // Act
+            actual = HW20.IsSpecial(str);
+
+            // Assert
+            Assert.AreEqual(expected, actual, "Expected " + expected + " but got " + actual);
+
+        }
+
+
+        [DataTestMethod]
+        [DynamicData("IsSpecialTestGenerator", DynamicDataSourceType.Method)]
+        public void IsSpecial_DynamicData(string str, bool expected)
+        {
+            // Arrange
+            bool actual;
+
+            // Act
+            actual = HW20.IsSpecial(str);
+
+            // Assert
+            Assert.AreEqual(expected, actual, "Expected " + expected + " but got " + actual);
+
+        }
+
+        public static object[][] IsSpecialTestGenerator()
+        {
+            return new[]
+            {
+                new object[] {null,false},
+                new object[] {"" , false},
+                new object[] {"12@34%!56" , false},
+                new object[] {"AbC23cBa1@" , true},
+                new object[] {"AbC23CBa1@" , false},
+
+            };
+        }
+
+    }
+}
---
+++ HomeWork/HW20.cs
@@ -0,0 +1,57 @@
+﻿namespace HomeWork
+{
+    public class HW20
+    {
+
+        public static bool IsSpecial(string str)
+        {
+
+            if (str == null || str.Length == 0) return false;
+
+            int numOfLetters = 26;
+            int[] arrOfSmallLetters = new int[numOfLetters];
+            int[] arrOfBigLetters = new int[numOfLetters];
+
+            for (int i = 0; i < numOfLetters; i++)
+            {
+                // start all indexes with zero
+                arrOfSmallLetters[i] = 0;
+                arrOfBigLetters[i] = 0;
+            }
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] >= 'a' && str[i] <= 'z')
+                {
+                    // a = 97 in ascii
+                    int charInInt = (int)str[i] - 97;
+                    arrOfSmallLetters[charInInt]++;
+
+                }
+
+                if (str[i] >= 'A' && str[i] <= 'Z')
+                {
+                    // A = 65 in ascii
+                    int charInInt = (int)str[i] - 65;
+                    arrOfBigLetters[charInInt]++;
+
+                }
+
+            }
+
+            for (int i = 0; i < numOfLetters; i++)
+                if (arrOfSmallLetters[i] != arrOfBigLetters[i])
+                    return false;
+
+            // checks if there is atleast a letter;
+            bool isLetter = false;
+            for (int i = 0; i < numOfLetters; i++)
+                if ((arrOfSmallLetters[i] == arrOfBigLetters[i]) && arrOfSmallLetters[i] != 0)
+                    isLetter = true;
+
+            if (!isLetter) return false;
+
+            return true;
+        }
+    }
+}
