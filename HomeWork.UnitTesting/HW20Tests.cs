05af290c6af883224ae86a17bf112730746b508f
HasanOmar1 <r3domar1@gmail.com>
2025-06-12 17:41:28 +0300

    finished HW20Tests [added xml tests]

--- HomeWork.UnitTesting/HW20Tests.cs
+++ HomeWork.UnitTesting/HW20Tests.cs
@@ -1,10 +1,22 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace HomeWork.UnitTesting
 {
     [TestClass]
     public class HW20Tests
     {
+
+        private TestContext testContext;
+
+        public TestContext TestContext
+        {
+            get { return testContext; }
+            set { testContext = value; }
+        }
+
+
+
         [TestMethod]
         public void IsSpecial_StringIsAAAaa_ReturnsFalse()
         {
@@ -27,7 +39,7 @@ namespace HomeWork.UnitTesting
         [DataRow("", false, DisplayName = "IsSpecial_StrIsEmpty_ReturnsFalse")]
         [DataRow("12@34%!56", false, DisplayName = "IsSpecial_StrIsNotLetters_ReturnsFalse")]
         [DataRow("AbC23cBa1@", true, DisplayName = "IsSpecial_StrIsValid_ReturnsTrue")]
-        [DataRow("AbC23CBa1@", false, DisplayName = "IsSpecial_StrIsInValid_ReturnsFalse")]
+        [DataRow("AbC23CBa1@", false, DisplayName = "IsSpecial_StrIsInvalid_ReturnsFalse")]
         public void IsSpecial_DataRow(string str, bool expected)
         {
             // Arrange
@@ -68,6 +80,25 @@ namespace HomeWork.UnitTesting
                 new object[] {"AbC23CBa1@" , false},
 
             };
+        }
+
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+                    @"|DataDirectory|\XMLTests\HW20Tests.xml", "IsSpecialTests", DataAccessMethod.Sequential)]
+        public void IsSpecial_XML()
+        {
+            // Arrange
+            bool actual;
+            string str = Convert.ToString(TestContext.DataRow["str"]);
+            bool expected = Convert.ToBoolean(TestContext.DataRow["expectedResult"]);
+
+            // Act
+            actual = HW20.IsSpecial(str);
+
+            // Assert
+            Assert.AreEqual(expected, actual, "Expected " + expected + " but got " + actual);
+
         }
 
     }
