f0181dbeb876e59ce5920acaded1edd31eb3ad39
HasanOmar1 <r3domar1@gmail.com>
2025-06-12 18:29:16 +0300

    added HW21

---
+++ HomeWork.UnitTesting/HW21Tests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace HomeWork.UnitTesting
+{
+    [TestClass]
+    public class HW21Tests
+    {
+
+        private TestContext testContext;
+
+        public TestContext TestContext
+        {
+            get { return testContext; }
+            set { testContext = value; }
+        }
+
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+                    @"|DataDirectory|\XMLTests\HW21Tests.xml",
+            "IsSpecialString", DataAccessMethod.Sequential)]
+        public void IsSpecial_XML()
+        {
+            // Arrange
+            bool actual;
+            bool expected = Convert.ToBoolean(TestContext.DataRow["expectedResult"]);
+            string str = Convert.ToString(TestContext.DataRow["string"]);
+
+            // Act
+            actual = HW21.isSpecial(str);
+
+            // Assert
+            Assert.AreEqual(expected, actual, "Expected " + expected + " but got " + actual);
+
+
+        }
+    }
+}
---
+++ HomeWork/HW21.cs
@@ -0,0 +1,69 @@
+﻿namespace HomeWork
+{
+    public class HW21
+    {
+
+        public static bool letterCounter(string str)
+        {
+            for (int i = 0; i < str.Length; i++)
+            {
+                int letterCounter = 0;
+                for (int j = 0; j < str.Length; j++)
+                {
+                    if (str[i] == str[j])
+                        letterCounter++;
+                }
+
+                if (letterCounter % 2 != 0)
+                    return false;
+
+            }
+
+            return true;
+        }
+
+        public static bool isSpecial(string str)
+        {
+            if (str == null || str.Length == 0 || str.Length % 2 != 0) return false;
+
+            // checks if a char is not a letter
+            for (int i = 0; i < str.Length; i++)
+                if (!char.IsLetter(str[i]))
+                    return false;
+
+            bool isSmall = true;
+            bool isBig = true;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsUpper(str[i]))
+                    isSmall = false;
+
+                if (char.IsLower(str[i]))
+                    isBig = false;
+            }
+
+
+            // string contains big and small letters
+            if (!isSmall && !isBig)
+                return false;
+
+            // string contains only small letters
+            if (isSmall && !isBig)
+            {
+                if (!letterCounter(str))
+                    return false;
+            }
+            else if (isBig && !isSmall)
+            // string contains only big letters
+            {
+                if (!letterCounter(str))
+                    return false;
+            }
+
+
+
+
+            return true;
+        }
+    }
+}
